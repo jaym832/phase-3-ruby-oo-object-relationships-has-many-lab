05f576d2c2e88293c442c0a05fe6306a3a9c7b48
Javier Marin <jjmarin777@gmail.com>
2021-05-20 15:43:33 -0500

    commit tree ea6eba7a094617ab8e0f87b9357bda096e7d9bde

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Song
+attr_accessor  :name , :artist
+
+@@all = []
+
+    def initialize (name)
+
+    @name=name
+    @@all << self
+    # binding.pry
+
+    end
+
+    
+    def self.all
+        @@all
+    end
+
+
+
+
+
+
+
+
+
+end
